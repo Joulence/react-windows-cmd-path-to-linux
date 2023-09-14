cce61303ecabd8fb29f451085873d5fbba0f4b7c
Joulence <4erviken@gmail.com>
2023-09-14 20:21:17 +0300

    App finished

--- src/components/InputPath.js
+++ src/components/InputPath.js
@@ -7,18 +7,34 @@ function InputPath() {
 
   const convertButtonHandler = (event) => {
     event.preventDefault();
-    console.log("Test");
+
+    if (linuxPath === "") {
+      setLinuxPath(windowsPath.replace(/\//g, "\\"));
+    }
+    if (windowsPath === "") {
+      setWindowsPath(linuxPath.replace(/\\/g, "/"));
+    }
   };
 
   return (
     <div className={styles.input}>
       <div>
         <label>Input Windows Path</label>
-        <input type="text" placeholder="Windows path...." />
+        <input
+          type="text"
+          placeholder="Windows path...."
+          onChange={(element) => setWindowsPath(element.target.value)}
+          value={windowsPath}
+        />
       </div>
       <div>
         <label>Input Linux Path</label>
-        <input type="text" placeholder="Linux path...." />
+        <input
+          type="text"
+          placeholder="Linux path...."
+          onChange={(element) => setLinuxPath(element.target.value)}
+          value={linuxPath}
+        />
       </div>
       <button onClick={convertButtonHandler} className={styles.btn}>
         Convert
